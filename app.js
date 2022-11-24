2538565b2ad317a8af8300a9aee600275f989ee9
ep7 <eduardo42400@gmail.com>
2022-11-23 23:00:13 -0600

    Added sign up section

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// const toggle_in_btn = document.querySelectorAll(".toggle-in");
+// const toggle_up_btn = document.querySelectorAll(".toggle-up");
+// const main = document.querySelector("main");
+
+// toggle_in_btn.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         main.classList.toggle("sign-up-mode");
+//     });
+// });
+
+// toggle_up_btn.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         main.classList.toggle("sign-in-mode");
+//     });
+// });
+
+// const toggleTo
 
